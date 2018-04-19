c8652d9546ee89ebdb22dddf0da2defc7cd27e6b
adia566 <566hayda@gmail.com>
2018-04-19 10:21:10 +0200

    Version Final Independiente html 1
    Independiente de html

--- js/slider.js
+++ js/slider.js
@@ -13,7 +13,6 @@ var t_int;
 
 //Funciones.
 function ocultaImgs(){
-
   for(a=0; a<elementos.length; a++){
     elementos[a].style.visibility = "hidden";
   }
@@ -21,54 +20,50 @@ function ocultaImgs(){
 
 function muestraPrimera(){
   ocultaImgs();
-
   elementos[0].style.visibility = "visible";
 }
 
 function sigImagen(){
   elementos[i].style.visibility = "hidden";
 
-  if(i+1 > elementos.length-1)
+  if(i+1 > elementos.length-1){
     i=0;
-  else
+  } else{
     i++;
+  }
 
   elementos[i].style.visibility = "visible";
-
   setNumImg();
 }
 
 function antImagen(){
   elementos[i].style.visibility = "hidden";
 
-  if(i-1 < 0)
+  if(i-1 < 0){
     i=elementos.length-1;
-  else
+  } else{
       i--;
+  }
 
   elementos[i].style.visibility = "visible";
-
   setNumImg();
 }
 
 function setNumImg(){
   nimg.readOnly = true;
-
   nimg.value = i+1 + " de " + elementos.length;
 }
 
 function playPause(){
-
   if(intervalo==false){
-    intervalo = setInterval(sigImagen, t_int);
-
     bpp.innerHTML = "Pausa";
+
+    intervalo = setInterval(sigImagen, t_int);
   }else{
-    clearInterval(intervalo);
-
-    intervalo = false;
-
     bpp.innerHTML = "Play";
+
+    clearInterval(intervalo);
+    intervalo = false;
   }
 }
 
@@ -83,22 +78,39 @@ function cambiarInt(){
 
 //Inicio.
 window.onload = function() {
-  bant = document.getElementById("bant");
-  bsig = document.getElementById("bsig");
-  bpp = document.getElementById("bpp");
-  nint = document.getElementById("bint");
-  nimg = document.getElementById("bimg");
-
   elementos = document.getElementsByTagName("img");
 
   i=0;
   t_int = 2000;
   intervalo = false;
 
-  setNumImg();
-  bant.innerHTML = "Anterior";
-  bsig.innerHTML = "Siguiente";
-  bpp.innerHTML = "Pausa";
+  bant = document.createElement("buttom");
+  bsig = document.createElement("buttom");
+  bpp = document.createElement("buttom");
+  nint = document.createElement("input");
+  nimg = document.createElement("input");
+
+  var contenido = document.createTextNode("Anterior");
+  bant.appendChild(contenido);
+
+  contenido = document.createTextNode("Siguiente");
+  bsig.appendChild(contenido);
+
+  contenido = document.createTextNode("Pausa");
+  bpp.appendChild(contenido);
+
+  contenido = document.createTextNode("");
+  nint.appendChild(contenido);
+
+  contenido = document.createTextNode(elementos.length);
+  nimg.appendChild(contenido);
+
+  document.body.appendChild(bant);
+  document.body.appendChild(bsig);
+  document.body.appendChild(bpp);
+  document.body.appendChild(nint);
+  document.body.appendChild(nimg);
+
   nint.value = t_int;
 
   bsig.onclick = sigImagen;
