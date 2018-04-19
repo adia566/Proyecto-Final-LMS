0d249544f21e54c3ae2e00e0f5e365bb988d70b1
adia566 <566hayda@gmail.com>
2018-04-19 02:33:59 +0200

    terminado
    terminado

--- js/slider.js
+++ js/slider.js
@@ -1,113 +1,100 @@
-window.onload = function() {
+// Variables.
+var bant;
+var bsig;
+var bpp
+var nint
+var nimg
 
-  var bant = document.getElementById("bant");
-  var bsig = document.getElementById("bsig");
-  var bpp = document.getElementById("bpp");
-  var nint = document.getElementById("bint");
-  var nimg = document.getElementById("bimg");
+var elementos;
 
-  var elementos = document.getElementsByTagName("img");
+var i;
+var intervalo;
+var t_int;
 
-  var i=0;
+//Funciones.
 
-  //Funciones
+function ocultaImgs(){
 
-  function ocultaImg(){
-
-    for(a=0; a<elementos.length; a++){
-
-      elementos[a].style.visibility = "hidden";
-    }
-
-    console.log("ocultaImg");
-
+  for(a=0; a<elementos.length; a++){
+    elementos[a].style.visibility = "hidden";
   }
+}
 
-  function muestraPrimera(){
+function muestraPrimera(){
+  ocultaImgs();
 
-    ocultaImg();
+  elementos[0].style.visibility = "visible";
+}
 
-    elementos[0].style.visibility = "visible";
+function sigImagen(){
+  elementos[i].style.visibility = "hidden";
 
-    console.log("muestraPrimera");
-  }
+  if(i+1 > elementos.length-1)
+    i=0;
+  else
+    i++;
 
-  function sigImagen(){
+  elementos[i].style.visibility = "visible";
+}
 
-    elementos[i].style.visibility = "hidden";
+function antImagen(){
+  elementos[i].style.visibility = "hidden";
 
-    if(i+1 > elementos.length-1)
-      i=0;
-    else
-      i++;
+  if(i-1 < 0)
+    i=elementos.length-1;
+  else
+      i--;
 
     elementos[i].style.visibility = "visible";
+}
 
-    console.log("sigImagen");
+function setNumImg(){
+  nimg.readOnly = true;
 
+  nimg.value = elementos.length + " imagenes";
+}
+
+function playPause(){
+  if(intervalo==false)
+    intervalo = setInterval(sigImagen, t_int);
+
+  else{
+    clearInterval(intervalo);
+
+    intervalo = false;
   }
+}
 
-  function antImagen(){
+function cambiarInt(){
+  bpp.click();
 
-    elementos[i].style.visibility = "hidden";
+  t_int = nint.value;
 
-    if(i-1 < 0)
-      i=elementos.length-1;
-    else
-        i--;
+  bpp.click();
+}
 
-      elementos[i].style.visibility = "visible";
-  }
 
-  function setNumImg(){
-    nimg.readOnly = true;
-    nimg.value = elementos.length + " imagenes";
-  }
+window.onload = function() {
+  bant = document.getElementById("bant");
+  bsig = document.getElementById("bsig");
+  bpp = document.getElementById("bpp");
+  nint = document.getElementById("bint");
+  nimg = document.getElementById("bimg");
 
-  function setNumInt(){
-    nint.vale = intervalo.seconds;
-  }
+  elementos = document.getElementsByTagName("img");
 
-  function playPause(){
-
-    if(intervalo==false)
-      intervalo = setInterval(sigImagen, t_int);
-
-    else{
-      clearInterval(intervalo);
-
-      intervalo = false;
-    }
-
-  }
-
-  function cambiarInt(){
-
-    bpp.click();
-
-    t_int = nint.value;
-
-    bpp.click();
-
-  }
-
-  ////////////////////////////////////////////////////////////
-  var intervalo;
-  var t_int = 3000;
+  i=0;
+  t_int = 3000;
   intervalo = false;
+
+  setNumImg();
+  nint.value = t_int;
 
   bsig.onclick = sigImagen;
   bant.onclick = antImagen;
   bpp.onclick = playPause;
-
-  nint.value = t_int;
-
   nint.onchange = cambiarInt;
 
   muestraPrimera();
-  setNumImg();
-  setNumInt();
   bpp.click();
-
-
 }
